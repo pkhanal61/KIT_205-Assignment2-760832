1a1c45f48c48d296536a3f9c2ee327d6717c0244
pkhanal61 <prasannaofficial61@gmail.com>
2026-05-31 11:11:48 +1000

    Define Dijkstra result struct

---
+++ dijkstra.h
@@ -0,0 +1,21 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+/*
+It returns shortest distances from a source node to all others,
+and the predecessor array for path reconstruction.
+Also it records nodes visited so we can compare with A*.
+*/
+
+#include "graph.h"
+
+#define INF_DIST 1e18   /* no path found */
+
+typedef struct {
+    double *dist;           // dist[i]  = shortest distance to node i 
+    int    *prev;           // prev[i]  = predecessor index (-1 = none)
+    int     nodes_visited;  // how many nodes were popped from the PQ   
+    int     num_nodes;      // length of dist[] and prev[]              
+} DijkstraResult;
+
+#endif
